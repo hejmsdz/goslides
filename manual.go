60f71f5a0e654ad1160a435a3b417bebfc3c3037
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2021-03-27 14:00:47 +0100

    :sparkles: add image url to manual

--- manual.go
+++ manual.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kjk/notionapi"
 )
 
 type Manual struct {
 	Steps []string `json:"steps"`
+	Image string   `json:"image"`
 }
 
 const manualPageID = "e831bd736718447e8177ade7435337cb"
 
 func GetManual(authToken string) (Manual, bool) {
-	m := Manual{make([]string, 0)}
+	m := Manual{make([]string, 0), ""}
 	client := &notionapi.Client{AuthToken: authToken}
 	page, err := client.DownloadPage(manualPageID)
 
@@ -19,9 +22,14 @@ func GetManual(authToken string) (Manual, bool) {
 		return m, false
 	}
 
-	for _, block := range page.Root().Content {
-		m.Steps = append(m.Steps, extractText(block.GetTitle()))
-	}
+	page.ForEachBlock(func(block *notionapi.Block) {
+		switch block.Type {
+		case notionapi.BlockNumberedList:
+			m.Steps = append(m.Steps, extractText(block.GetTitle()))
+		case notionapi.BlockImage:
+			m.Image = fmt.Sprintf("%s?table=block&id=%s", block.ImageURL, block.ID)
+		}
+	})
 
 	return m, true
 }
