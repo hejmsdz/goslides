2cd75460e9ebaea98a79c645c906abcf8ed55ae2
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2025-04-18 09:03:38 +0200

    :sparkles: add support and contact urls to bootstrap

--- dtos/bootstrap_dtos.go
+++ dtos/bootstrap_dtos.go
@@ -1,7 +1,9 @@
 package dtos
 
 type BootstrapResponse struct {
-	CurrentVersion string `json:"currentVersion"`
-	AppDownloadURL string `json:"appDownloadUrl"`
-	SongEditURL *string `json:"songEditUrl"`
+	CurrentVersion string  `json:"currentVersion"`
+	AppDownloadURL string  `json:"appDownloadUrl"`
+	SongEditURL    *string `json:"songEditUrl"`
+	ContactURL     *string `json:"contactUrl"`
+	SupportURL     *string `json:"supportUrl"`
 }
